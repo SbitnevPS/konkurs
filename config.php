45cd32e0cbc919ac888bdb0fc4a3b08890050f61
SbitnevPS <sbitnev.ps@bk.ru>
2026-04-18 16:44:13 +0300

    Update config.php

--- config.php
+++ config.php
@@ -8,9 +8,9 @@ return [
     'app_name' => 'konkurs',
     'db' => [
         'host' => getenv('DB_HOST') ?: '127.0.0.1',
-        'name' => getenv('DB_NAME') ?: 'konkurs',
-        'user' => getenv('DB_USER') ?: 'root',
-        'pass' => getenv('DB_PASS') ?: '',
+        'name' => getenv('DB_NAME') ?: 'pavel-sbitnev_test',
+        'user' => getenv('DB_USER') ?: '046613795_test',
+        'pass' => getenv('DB_PASS') ?: 'RwfgwR5hnT7@',
         'charset' => getenv('DB_CHARSET') ?: 'utf8mb4',
     ],
     'vk_mini' => $vkMiniConfig,
